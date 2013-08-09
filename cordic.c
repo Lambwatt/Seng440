71650b251c6827da917544bd0ab751bfd13854b0
Michael Pattie <mjpattie@w216-209.wireless.uvic.ca>
2013-08-09 13:12:19 -0700

    Created naive cordic implimentation using floats.

--- cordic.c
+++ cordic.c
@@ -1,8 +1,6 @@
-#define X				0
-#define Y				1
-#define Z				2
 #define MAX_ITER		16
 #define SCALING_FACTOR	1.6468
+#define PI  3.14159
 
 #define ABS(x)		((x < 0) ? (-x) : x)
 
@@ -14,81 +12,59 @@ float arctan_degrees[MAX_ITER] = {45.0,26.56,14.04,7.13,
 								0.22,0.11,0.05,0.03,
 								0.000244,0.000122,0.000061,0.0000305};
 
-float new_values[3];
-
-
-float *rotation(float x, float y, float rotation_angle){
+void rotation(float *x, float *y, float *rotation_angle){
 	int i;
 	float last_x, last_y;
-	last_x = x;
-	last_y = y;
-	new_values[Z] = rotation_angle;
+	last_x = *x;
+	last_y = *y;
 	
 	for(i = 0; i < MAX_ITER; i++){
 		
-	
 		float d = 1;
-		if(new_values[Z] < 0) d = -1;
+		if(*rotation_angle < 0) d = -1;
 		
-		printf("Iteration: %i\td = %f\tx = %f\ty = %f\tz = %f\n",i,d,last_x,last_y,new_values[Z]);
-		
-		new_values[X] = last_x - ((d*last_y)/(1<<i));
-		new_values[Y] = last_y + ((d*last_x)/(1<<i));
-		new_values[Z] = new_values[Z] - (d*arctan_degrees[i]);
+		*x = last_x - ((d*last_y)/(1<<i));
+		*y = last_y + ((d*last_x)/(1<<i));
+		*rotation_angle -= (d*arctan_degrees[i]);
 	
-		
-		
-		last_x = new_values[X];
-		last_y = new_values[Y];
+		last_x = *x;
+		last_y = *y;
 	}
 	
-	new_values[X] /= SCALING_FACTOR;
-	new_values[Y] /= SCALING_FACTOR;
+	*x /= SCALING_FACTOR;
+	*y /= SCALING_FACTOR;
 	
-	return new_values;
 }
 
-float *vectoring(float x, float y, float z){
+void vectoring(float* x, float* y, float* z){
 	int i;
 	float last_x, last_y;
-	last_x = x;
-	last_y = y;
-	new_values[Z] = z;
+	last_x = *x;
+	last_y = *y;
 	
 	for(i = 0; i < MAX_ITER; i++){
 		
-	
 		float d = -1;
 		if(last_y < 0) d = 1;
 		
-		printf("Iteration: %i\td = %f\tx = %f\ty = %f\tz = %f\n",i,d,last_x,last_y,new_values[Z]);
-		
-		new_values[X] = last_x - ((d*last_y)/(1<<i));
-		new_values[Y] = last_y + ((d*last_x)/(1<<i));
-		new_values[Z] = new_values[Z] - (d*arctan_degrees[i]);
+		*x = last_x - ((d*last_y)/(1<<i));
+		*y = last_y + ((d*last_x)/(1<<i));
+		*z -= (d*arctan_degrees[i]);
 	
-		
-		
-		last_x = new_values[X];
-		last_y = new_values[Y];
+		last_x = *x;
+		last_y = *y;
 	}
 	
-	new_values[X] /= SCALING_FACTOR;
-	new_values[Y] /= SCALING_FACTOR;
-	
-	
-	
-	return new_values;
-	
+	*x /= SCALING_FACTOR;
+	*y /= SCALING_FACTOR;
 	
 }
 
 float arctan_fraction(float y, float x){
 	
-	float *r = vectoring(x,y,0);
-	
-	return r[Z];
-
+	float r = 0.0f; 
+	vectoring(&x,&y,&r);
+	return r;
 }
 
 float arctan(float x){
@@ -96,28 +72,47 @@ float arctan(float x){
 }
 
 float cos_vec(float angle){	
-	float *r = rotation(1,0,angle);
-	return r[X];
+	float r =  1.0f;
+	float y = 0.0f;
+	rotation(&r,&y,&angle);
+	return r;
 }
 
 float sin_vec(float angle){
-	float *r = rotation(1,0,angle);
-	return r[Y];
+	float r = 0.0f;
+	float x = 1.0;
+	rotation(&x,&r,&angle);
+	return r;
 }
 
 int main(void){
 	float angle[2] = {1,1};
 	
-	//float *result = rotation(1,0, 90);
-	float *result = vectoring(0,1,0);
+	float x = 1.0f;
+	float y = 0.0f;
+	float z = 90.0f;;
 	
-	printf("Results: x = %f\ty = %f\tz = %f\n",result[X],result[Y],result[Z]);
+	rotation(&x,&y, &z);
+	float f = arctan(0.5);
+
+	printf("math.arctan(0.5) = %f\n", (atan(0.5)*180.0/PI));
+	printf("Arctan(0.5) = %f\n",f);
+
+	printf("Results: x = %f\ty = %f\tz = %f\n",x,y,z);
+	printf("expected: x=0.0, y=1.0, z=0.0\n");
 	
-	//float f = arctan(0.5);
+	x = 0.0f;
+	y = 1.0f;
+	z = 0.0f;
 	
-	//printf("Arctan = %f\n",f);
+	vectoring(&x,&y,&z);	
+	f = sin_vec(45);
+
+	printf("math.sin(45) = %f\n",(sin(45.0*PI/180.0)*180.0/PI));
+	printf("sin = %f\n",f);
+
+	printf("Results: x = %f\ty = %f\tz = %f\n",x,y,z);
+	printf("expected: x=1.0, y=0.0, z=90.0\n");
 	
-	//float f = sin_vec(45);
-	//printf("sin = %f\n",f);
 	return 0;
 }
