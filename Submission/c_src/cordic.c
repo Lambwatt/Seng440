499a1b5f6462b759608f68832037d9de53d3487f
Michael Pattie <mjpattie@Michaels-MacBook-Air.local>
2013-08-17 19:03:36 -0700

    Creating testfile and evaluation counts for remaining files.

--- Submission/c_src/cordic.c
+++ Submission/c_src/cordic.c
@@ -2,29 +2,75 @@
 #include "cordic.h"
 #include <stdint.h>
 #include <stdio.h>
+#include <math.h>
+
+#define PI 3.14159265359
 
 int main(void){
 	int32_t temp = 0;
+	int16_t angle;
+	int i = 0;
+	float error;
+	float correctSin, correctCos;
 	
-	/*temp = arctan_fraction_routine(0, 0);
-	temp = arctan_routine(0);
-	temp = cos_routine(0);
-	temp = sin_routine(0);
+	for(;i<13;i++)
+	{
+		angle = 90 - i*15;
+		correctSin = sin(angle*PI/180);
+		correctCos = cos(angle*PI/180);
 
-	temp = arctan_fraction_inline(0, 0);
-	temp = arctan_inline(0);
-	temp = cos_inline(0);
-	temp = sin_inline(0);
+		printf("Correct values: Angle = %i, Sin = %f, Cos = %f", angle, corectSin, correctCos);
+
+		printf("\n\nSin results:\n");
+		printf("Function| Value		| Error		");
+
+		temp = sin_float(angle);
+		error = abs(correctSin - temp);
+		printf("Float		| %07f		|	%07f	\n",temp, error);
+		
+		temp = sin_routine(angle);
+		error = abs(correctSin - temp);
+		printf("Routine	| %07f		|	%07f	\n",temp, error);
 	
-	temp = arctan_fraction_macro(0, 0);
-	temp = arctan_macro(0);
-	temp = cos_macro(0);
-	temp = sin_macro(0);*/
+		temp = sin_inline(angle);
+		error = abs(correctSin - temp);
+		printf("Inline	| %07f		|	%07f	\n",temp, error);
 	
-	temp = cos_routine(0);
-	printf("%i\n",temp);
-	temp = cos_inline(0);
-	printf("%i\n",temp);
-	temp = cos_macro(0);
-	printf("%i\n",temp);
+		temp = sin_macro(angle);
+		error = abs(correctSin - temp);
+		printf("Macro		| %07f		|	%07f	\n",temp, error);
+		
+		temp = sin_optimized(angle);
+		error = abs(correctSin - temp);
+		printf("Optimizd| %07f		|	%07f	\n",temp, error);
+	
+		printf("\nCos results:\n");
+		printf("Function| Value		| Error		");
+
+		temp = cos_float(angle);
+		error = abs(correctCos - temp);
+		printf("Float		| %07f		|	%07f	\n",temp, error);
+		
+		temp = cos_routine(angle);
+		error = abs(correctCos - temp);
+		printf("Routine	| %07f		|	%07f	\n",temp, error);
+	
+		temp = cos_inline(angle);
+		error = abs(correctCos - temp);
+		printf("Inline	| %07f		|	%07f	\n",temp, error);
+	
+		temp = cos_macro(angle);
+		error = abs(correctCos - temp);
+		printf("Macro		| %07f		|	%07f	\n",temp, error);
+		
+		temp = cos_optimized(angle);
+		error = abs(correctCos - temp);
+		printf("Optimizd| %07f		|	%07f	\n",temp, error);
+	}	
+	//temp = cos_routine(0);
+	//printf("%i\n",temp);
+	//temp = cos_inline(0);
+	//printf("%i\n",temp);
+	//temp = cos_macro(0);
+	//printf("%i\n",temp);
 }
--- Submission/c_src/cordic.h
+++ Submission/c_src/cordic.h
@@ -4,13 +4,16 @@
 #include <stdint.h>
 
 
-
-									
+int16_t arctan_fraction_float(int16_t, int16_t);
+int16_t arctan_float(int16_t);
+int16_t cos_float(int32_t angle);
+int16_t sin_float(int32_t angle);
+						
 int16_t arctan_fraction_routine(int16_t, int16_t);
 int16_t arctan_routine(int16_t);
 int16_t cos_routine(int32_t angle);
 int16_t sin_routine(int32_t angle);
-
+			
 int16_t arctan_fraction_inline(int16_t, int16_t);
 int16_t arctan_inline(int16_t);
 int16_t cos_inline(int32_t angle);
@@ -20,5 +23,10 @@ int16_t arctan_fraction_macro(int16_t, int16_t);
 int16_t arctan_macro(int16_t);
 int16_t cos_macro(int32_t angle);
 int16_t sin_macro(int32_t angle);
+
+int16_t arctan_fraction_optimized(int16_t, int16_t);
+int16_t arctan_optimized(int16_t);
+int16_t cos_optimized(int32_t angle);
+int16_t sin_optimized(int32_t angle);
 
 #endif
